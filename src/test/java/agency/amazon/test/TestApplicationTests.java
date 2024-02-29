8c46dd2a2159415b73b51e9fa3d6b46a820c8c26
MorgothGorthaur <victortarasov13032001@gmail.com>
2024-02-29 21:39:01 +0200

    Initial commit

---
+++ src/test/java/agency/amazon/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package agency.amazon.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
