dd472ec3129acd8719ee3cbd63ca978c4344ca85
MorgothGorthaur <victortarasov13032001@gmail.com>
2024-03-02 01:55:35 +0200

    created batch processor

---
+++ batch/src/main/java/agency/amazon/test/batch/executor/BatchProcessorImpl.java
@@ -0,0 +1,31 @@
+package agency.amazon.test.batch.executor;
+
+import agency.amazon.test.batch.dto.ReaderConfigHolder;
+import agency.amazon.test.batch.exception.UnknownFileTypeException;
+import agency.amazon.test.batch.reader.Reader;
+import agency.amazon.test.batch.writer.ReportWriter;
+import agency.amazon.test.model.SalesAndTrafficReport;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+
+public class BatchProcessorImpl implements BatchProcessor {
+    private final Reader reader;
+    private final ReportWriter writer;
+    private final ReaderConfigHolder holder;
+
+    public BatchProcessorImpl(ReportWriter writer, ReaderConfigHolder holder, List<Reader> readers) {
+        this.writer = writer;
+        this.holder = holder;
+        this.reader = readers.stream()
+                .filter(v -> v.getType().equals(holder.getFileType())).findFirst()
+                .orElseThrow(() -> new UnknownFileTypeException(holder.getFileType()));
+    }
+    @Override
+    public void process() {
+        var report = reader.readData(holder.getFilePath(), SalesAndTrafficReport.class);
+        writer.saveToDb(report);
+    }
+}
