504e02f567b16ecd5239df5bdc78a36693d5531d
MorgothGorthaur <victortarasov13032001@gmail.com>
2024-03-01 17:08:25 +0200

    created account rest controller

---
+++ controller/src/main/java/agency/amazon/test/controller/AccountController.java
@@ -0,0 +1,30 @@
+package agency.amazon.test.controller;
+
+import agency.amazon.test.dto.AccountDto;
+import agency.amazon.test.dto.AccountWithDetailsDto;
+import agency.amazon.test.service.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/account")
+public class AccountController {
+    private final AccountService service;
+
+    @PostMapping("/add")
+    public void add(@RequestBody AccountWithDetailsDto dto) {
+        service.addAccount(dto);
+    }
+
+    @GetMapping("/get")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public AccountDto get(Principal principal){
+        return service.getAccount(principal.getName());
+    }
+
+
+}
