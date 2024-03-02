cd01f99591c797e91d7e8d00ee7bf30c3b9a4386
MorgothGorthaur <victortarasov13032001@gmail.com>
2024-03-02 13:13:55 +0200

    enabled caching

--- controller/src/main/java/agency/amazon/test/TestApplication.java
+++ controller/src/main/java/agency/amazon/test/TestApplication.java
@@ -2,9 +2,11 @@ package agency.amazon.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableCaching
 @EnableScheduling
 public class TestApplication {
 	public static void main(String[] args) {
