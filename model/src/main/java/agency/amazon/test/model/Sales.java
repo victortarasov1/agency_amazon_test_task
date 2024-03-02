2f164153b7cfd74c19f33e6c2788ce19ded547f6
MorgothGorthaur <victortarasov13032001@gmail.com>
2024-03-02 07:34:15 +0200

    added json include nonnull annotation

--- model/src/main/java/agency/amazon/test/model/Sales.java
+++ model/src/main/java/agency/amazon/test/model/Sales.java
@@ -1,10 +1,12 @@
 package agency.amazon.test.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Sales {
     private AmountAndCurrency orderedProductSales;
     private AmountAndCurrency orderedProductSalesB2B;
