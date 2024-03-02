4e72ebb252671b31297418813224a8b7ff88e695
MorgothGorthaur <victortarasov13032001@gmail.com>
2024-03-02 13:14:30 +0200

    added cacheable annotations

--- service/src/main/java/agency/amazon/test/service/report/SalesAndTrafficByAsinServiceImpl.java
+++ service/src/main/java/agency/amazon/test/service/report/SalesAndTrafficByAsinServiceImpl.java
@@ -3,6 +3,7 @@ package agency.amazon.test.service.report;
 import agency.amazon.test.model.SalesAndTraffic;
 import agency.amazon.test.repository.SalesAndTrafficRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -13,16 +14,19 @@ import java.util.List;
 public class SalesAndTrafficByAsinServiceImpl implements SalesAndTrafficService {
     private final SalesAndTrafficRepository repository;
     @Override
+    @Cacheable(value = "salesAndTraffic", key = "#asin")
     public SalesAndTraffic findById(String asin) {
         return repository.findById(asin).orElseGet(SalesAndTraffic::new);
     }
 
     @Override
-    public List<SalesAndTraffic> findAllById(List<String> asin) {
-        return repository.findAllById(asin);
+    @Cacheable(value = "salesAndTraffic", key = "#asins")
+    public List<SalesAndTraffic> findAllById(List<String> asins) {
+        return repository.findAllById(asins);
     }
 
     @Override
+    @Cacheable(value = "salesAndTraffic", key = "'findAll'")
     public List<SalesAndTraffic> findAll() {
         return repository.findAll().stream().filter(this::isAsinReport).toList();
     }
