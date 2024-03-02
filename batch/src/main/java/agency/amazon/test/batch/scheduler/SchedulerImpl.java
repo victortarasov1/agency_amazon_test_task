58ee6f26d0aad96bb7bb17c229afd3522250f725
MorgothGorthaur <victortarasov13032001@gmail.com>
2024-03-02 13:27:43 +0200

    removed unused fields

--- batch/src/main/java/agency/amazon/test/batch/scheduler/SchedulerImpl.java
+++ batch/src/main/java/agency/amazon/test/batch/scheduler/SchedulerImpl.java
@@ -1,7 +1,6 @@
 package agency.amazon.test.batch.scheduler;
 
 import agency.amazon.test.batch.executor.BatchProcessor;
-import agency.amazon.test.repository.SalesAndTrafficRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -14,7 +13,6 @@ import java.util.concurrent.TimeUnit;
 @PropertySource("classpath:batch.properties")
 public class SchedulerImpl implements Scheduler {
     private final BatchProcessor processor;
-    private final SalesAndTrafficRepository repository;
 
     @Override
     @Scheduled(fixedDelay = 1, timeUnit = TimeUnit.MINUTES)
